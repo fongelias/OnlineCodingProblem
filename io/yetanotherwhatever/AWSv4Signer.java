f7407e11dffe1754ea0a78b9e0128ff00e9f135b
Andrew Chang <andrew_chang1@hotmail.com>
2016-10-13 14:48:20 -0400

    adding signing and lambda code

---
+++ io/yetanotherwhatever/AWSv4Signer.java
@@ -0,0 +1,101 @@
+package io.yetanotherwhatever;
+
+import sun.misc.BASE64Encoder;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+
+/*
+Based on
+http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-authentication-HTTPPOST.html
+http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-HTTPPOSTConstructPolicy.html
+http://docs.aws.amazon.com/AmazonS3/latest/API/sig-v4-authenticating-requests.html
+http://docs.aws.amazon.com/AmazonS3/latest/API/sigv4-UsingHTTPPOST.html
+ */
+
+
+            public class AWSv4Signer {
+
+                static byte[] HmacSHA256(String data, byte[] key) throws Exception  {
+                    return HmacSHA256(data.getBytes("UTF-8"), key);
+                }
+
+
+                static byte[] HmacSHA256(byte[] data, byte[] key) throws Exception  {
+                    String algorithm="HmacSHA256";
+                    Mac mac = Mac.getInstance(algorithm);
+                    mac.init(new SecretKeySpec(key, algorithm));
+                    return mac.doFinal(data);
+                }
+
+                /*
+                based on http://docs.aws.amazon.com/general/latest/gr/sigv4-calculate-signature.html
+                 */
+                static byte[] getSignatureKey(String key, String dateStamp, String regionName, String serviceName) throws Exception  {
+                    byte[] kSecret = ("AWS4" + key).getBytes("UTF8");
+                    byte[] kDate    = HmacSHA256(dateStamp, kSecret);
+                    byte[] kRegion  = HmacSHA256(regionName, kDate);
+                    byte[] kService = HmacSHA256(serviceName, kRegion);
+                    byte[] kSigning = HmacSHA256("aws4_request", kService);
+                    return kSigning;
+                }
+
+                public static void main(String[] args) {
+
+
+                    String aws_secret_key = args[1];
+                    String algorithm = "AWS4-HMAC-SHA256";
+                    String dateStamp = "20161013";
+                    String region = "us-east-1";
+                    String serviceName = "s3";
+                    String bucket = "public.yetanotherwhatever.io";
+                    String accessKeyID = args[0];
+
+                            String policy_document = "{\n" +
+                            "  \"expiration\":\"2018-01-01T00:00:00Z\",\n" +
+                            "  \"conditions\": [\n" +
+                            "    {\"bucket\":\"" + bucket + "\"},\n" +
+                            "    [\"starts-with\",\"$key\",\"uploads/\"],\n" +
+                            "    {\"acl\":\"private\"},\n" +
+                            "    {\"success_action_redirect\":\"http://yetanotherwhatever.io/submitting.html\"},\n" +
+                            "    {\"x-amz-algorithm\":\"" + algorithm + "\"},\n" +
+                            "    {\"x-amz-credential\":\"" + accessKeyID + "/" + dateStamp + "/" + region + "/" + serviceName + "/aws4_request\"},\n" +
+                            "    {\"x-amz-date\":\"" + dateStamp + "T000000Z\"},\n" +
+                            "    {\"x-amz-storage-class\":\"REDUCED_REDUNDANCY\"},\n" +
+                            "    [\"content-length-range\",0,1048576]\n" +
+                            "  ]\n" +
+                            "}";
+
+                    try {
+
+                        policy_document = policy_document.replaceAll("\\s", "");
+                        String b64_policy_doc = (new BASE64Encoder()).encode(
+                                policy_document.getBytes("UTF-8"));
+
+
+            byte[] signatureKey = getSignatureKey(aws_secret_key, dateStamp, region, serviceName);
+
+            String stringToSign = b64_policy_doc;
+            byte[] signingKey = signatureKey;
+
+            byte[] hash = HmacSHA256(stringToSign, signingKey);
+            final StringBuilder builder = new StringBuilder();
+            for(byte b : hash) {
+                builder.append(String.format("%02x", b));
+            }
+            String signature = builder.toString();
+
+
+            System.out.println("string to sign");
+            System.out.println(stringToSign);
+            System.out.println("sig");
+            System.out.println(signature);
+
+        }
+        catch(Exception e)
+        {
+
+        }
+
+    }
+}
---
+++ io/yetanotherwhatever/processCodingProblemSolutionUpload.java
@@ -0,0 +1,219 @@
+package io.yetanotherwhatever;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.StorageClass;
+
+public class processCodingProblemSolutionUpload implements RequestHandler<S3Event, String> {
+
+    @Override
+    public String handleRequest(S3Event input, Context context) {
+
+    	LambdaLogger logger = context.getLogger();
+    	
+        for (S3EventNotificationRecord s : input.getRecords())
+        {
+        	Instant previous, current;
+        	long parseMs = 0, compareMs = 0, saveMs = 0;
+        	
+        	previous = Instant.now();
+        	    
+        	String bucketName = s.getS3().getBucket().getName();
+        	String key = s.getS3().getObject().getKey();
+        	String prefix = null;
+        	String plainKey = "";
+        	String parentFolder = "uploads/";
+        	int idx = key.indexOf(parentFolder);
+        	if (idx == -1)
+        	{
+        		//unexpected
+        		logger.log("Expected parent \"" + parentFolder + "\"folder not found");
+        		return null;
+        	}
+        	plainKey = key.substring(idx + parentFolder.length());
+        	idx = plainKey.indexOf('/');
+        	if (idx != -1)
+        	{
+        		prefix = plainKey.substring(0, idx);
+        		plainKey = plainKey.substring(idx + 1);
+        	}
+        	else
+        	{
+        		//unexpected
+        		logger.log("No key prefix found");
+        		return null;
+        	}
+
+        	String nl = System.lineSeparator();
+        	logger.log("S3 bucket: " + bucketName + nl);
+        	logger.log("S3 key: " + key + nl);
+        	logger.log("S3 prefix: " + prefix + nl);
+        	logger.log("S3 plain key: " + plainKey + nl);
+            String slnKey = "solutions/" + prefix +  "-out.txt";
+            logger.log("S3 solution key: " + slnKey + nl);
+            
+        	current = Instant.now();
+        	if (previous != null) {
+        		parseMs = ChronoUnit.MILLIS.between(previous,current);
+        	    previous = current;
+        	}
+            
+            logger.log("Reading file: " + key + nl);
+        	AmazonS3 client = new AmazonS3Client();
+            S3Object putFile = client.getObject(bucketName, key);
+            InputStream putContents = putFile.getObjectContent();
+
+            logger.log("Reading file: " + slnKey + nl);            
+            S3Object slnFile = client.getObject(bucketName, slnKey);
+            InputStream slnContents = slnFile.getObjectContent();
+         
+            String result =  compareStreams(putContents, slnContents);
+        	
+            current = Instant.now();
+        	if (previous != null) {
+        		compareMs = ChronoUnit.MILLIS.between(previous,current);
+        	    previous = current;
+        	}
+            
+            logger.log("Result: " + result + nl);            
+            saveResultToS3(result, "results/" + plainKey + ".html", logger);
+            
+        	current = Instant.now();
+        	if (previous != null) {
+        		saveMs = ChronoUnit.MILLIS.between(previous,current);
+        	    previous = current;
+        	}
+        	
+        	logger.log("Time to parse = " + parseMs + " ms.");
+        	logger.log("Time to compare = " + compareMs + " ms.");
+        	logger.log("Time to save = " + saveMs + " ms.");
+        	logger.log("Total time = " + (parseMs + compareMs + saveMs) + " ms.");
+        	
+            return result;
+        }
+        
+        return null;
+    }
+    
+    private void saveResultToS3 (String result, String keyName, LambdaLogger logger) {
+
+        final String bucketName     = "yetanotherwhatever.io";
+        AmazonS3Client s3client = new AmazonS3Client(new DefaultAWSCredentialsProviderChain());
+        String nl = System.lineSeparator();        
+        
+        String content = "<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /></head><body lang=EN-US><div class=mainDiv><p class=result>" +
+        		result + "</p></div></body></html>";
+
+    	logger.log("Saving result to: " + keyName + nl);
+            try {          
+                InputStream stream = new ByteArrayInputStream(
+                		content.getBytes(StandardCharsets.UTF_8));
+                String bucket = bucketName;
+                ObjectMetadata metadata = new ObjectMetadata();
+                metadata.setContentType("text/html");
+                PutObjectRequest put = new PutObjectRequest(bucket, keyName,
+                        stream, metadata).withCannedAcl(CannedAccessControlList.PublicRead);
+                put.setStorageClass(StorageClass.ReducedRedundancy);
+                put.setMetadata(metadata);
+                s3client.putObject(put);
+                               
+
+            } catch (AmazonServiceException ase) {
+                logger.log("Caught an AmazonServiceException, which " +
+                        "means your request made it " +
+                        "to Amazon S3, but was rejected with an error response" +
+                        " for some reason." + nl);
+                logger.log("Error Message:    " + ase.getMessage() + nl);
+                logger.log("HTTP Status Code: " + ase.getStatusCode() + nl);
+                logger.log("AWS Error Code:   " + ase.getErrorCode() + nl);
+                logger.log("Error Type:       " + ase.getErrorType() + nl);
+                logger.log("Request ID:       " + ase.getRequestId() + nl);
+            } catch (AmazonClientException ace) {
+                logger.log("Caught an AmazonClientException, which " +
+                        "means the client encountered " +
+                        "an internal error while trying to " +
+                        "communicate with S3, " +
+                        "such as not being able to access the network." + nl);
+                logger.log("Error Message: " + ace.getMessage() + nl);
+                logger.log(ace.getStackTrace().toString() + nl);
+            } catch (Throwable t) {
+            	logger.log("Error encountered while saving results." + nl);
+            	t.printStackTrace();
+            }
+    }
+    
+    private static String compareStreams(InputStream attempt, InputStream ref)
+    {
+        BufferedReader aIn = new BufferedReader(new InputStreamReader(attempt));
+        BufferedReader rIn = new BufferedReader(new InputStreamReader(ref));
+        String aLine = null;
+        String rLine = null;
+
+        //print first 3 lines
+        int i = 1;
+        while(i < 10000)
+        {
+        	try
+        	{
+        		aLine = aIn.readLine();
+        		if (aLine != null) aLine = aLine.trim();
+        		rLine = rIn.readLine();
+        		if (rLine != null) rLine = rLine.trim();
+        		String nl = System.lineSeparator();
+        		if (aLine != null && !aLine.equals(rLine))
+        		{
+        			return "Your solution is incorrect." + nl
+        					+ "The first incorrect line is line " + i + "." + nl
+        					//+ "Expected: \"" + rLine + "\"" + nl
+        					//+ "but read \"" + aLine + "\""
+        					+ "";
+        		}
+        		else if (aLine == null && rLine == null)
+        		{
+        			return "Success!!  Your solution is correct!";
+        		}
+        		else if (aLine == null)
+        		{
+        			return "Your solution is incorrect." + nl
+        					+ "File too short.";
+        		}
+        		else if (rLine == null)
+        		{
+        			return "Your solution is incorrect." + nl
+        					+ "File too long.";
+        		}
+        	}
+        	catch (IOException e)
+        	{
+        		return "Unexpected error on line " + i;
+        	}
+        	
+        	i++;
+        }
+        
+        return "Max file lines exceeded.";
+    }
+
+}
+ 
