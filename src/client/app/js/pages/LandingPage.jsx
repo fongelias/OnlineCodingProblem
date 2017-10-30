594d1a8325e5e8c67418798ba632f1490f600c36
fongelias <fongelias@gmail.com>
2017-10-30 10:41:56 -0400

    Updated text on landing page to include end date, added constants import

--- src/client/app/js/pages/LandingPage.jsx
+++ src/client/app/js/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import constants from '../constants.js';
 //Components
 import RegistrationForm from '../Components/RegistrationForm.jsx';
 
@@ -8,11 +9,16 @@ export default class LandingPage extends Component {
 		return (
 			<div className="pageBody landingPage">
 				<div className="card blue">
-					<h1>Symantec Interview Portal</h1>
-					<p>Register for an account to begin your interview</p>
+					<h1>Symantec Cambridge, Norton Cloud Services Team</h1>
+					<p>2018 Summer Internship Online Coding Problem</p>
 				</div>
-				<div className="card center">
-					<p>Enter your information below:</p>
+				<div className="card">
+					<p>Thank you for your interest in Symantec!</p>
+					<p>The first step in our interview process will be two take-home coding exercises.</p>
+					<p>The second step is a one-hour onsite interview (remote is also possible for students unable to visit our Cambridge office).</p>
+					<p>Please register below to receive your personalized link containing further instructions.</p>
+					<p className="text italic">Deadline: the Online Coding Problem portion of our 2018 Summer Internship search will be closing on {constants.campaign.endDateString}</p>
+					<p>Thanks!</p>
 					<RegistrationForm registerUser={this.props.registerUser}/>
 				</div>
 			</div>
