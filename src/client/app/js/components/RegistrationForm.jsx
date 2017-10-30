bf3ea04162783500a24f2bd61e5636453a1e6043
fongelias <fongelias@gmail.com>
2017-10-30 10:38:12 -0400

    added state for button label as well as disabled state. Added binding for activateSubmit. Added activateSubmit() for disabling buttons based on checkboxes

--- src/client/app/js/components/RegistrationForm.jsx
+++ src/client/app/js/components/RegistrationForm.jsx
@@ -5,6 +5,12 @@ export default class RegistrationForm extends Component {
 		super();
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.activateSubmit = this.activateSubmit.bind(this);
+
+		this.state = {
+			submitButtonLabel: "",
+			submitButtonDisabled: true,
+		};
 	}
 
 	handleSubmit() {
@@ -12,10 +18,22 @@ export default class RegistrationForm extends Component {
 		const lastName = this.refs.lastName.value;
 		const email = this.refs.email.value;
 
-
-
-
 		this.props.registerUser(firstName, lastName, email);
+	}
+
+	activateSubmit() {
+		const enrollmentStatus = this.refs.enrollmentStatusCheckbox.checked;
+		const graduationStatus = this.refs.graduationStatusCheckbox.checked;
+
+		if(graduationStatus && enrollmentStatus) {
+			this.setState({
+				submitButtonDisabled: false,
+			})
+		} else {
+			this.setState({
+				submitButtonDisabled: true,
+			})
+		}
 	}
 
 	render() {
@@ -24,7 +42,18 @@ export default class RegistrationForm extends Component {
 				<input ref="firstName" type="text" placeholder="First Name"/>
 				<input ref="lastName" type="text" placeholder="Last Name" />
 				<input ref="email" type="email" placeholder="Email" />
-				<button className="submitButton" onClick={this.handleSubmit}>Submit your registration</button>
+				<div className="input-group">
+					<button ref="submitButton" className="submitButton" onClick={this.handleSubmit} disabled={this.state.submitButtonDisabled}>Submit your registration</button>
+					<label htmlFor="submitButton">{this.state.submitButtonLabel}</label>
+				</div>
+				<div className="input-group">
+					<input ref="enrollmentStatusCheckbox" type="checkbox" id="enrollmentStatusCheckbox" name="enrollmentStatus" onClick={this.activateSubmit}/>
+					<label htmlFor="enrollmentStatusCheckbox">I will be enrolled as a full-time student in the semester beginning in Fall 2018</label>
+				</div>
+				<div className="input-group">
+					<input ref="graduationStatusCheckbox" type="checkbox" id="graduationStatusCheckbox" name="enrollmentStatus" onClick={this.activateSubmit}/>
+					<label htmlFor="graduationStatusCheckbox">I will be graduating by the summer of 2019</label>
+				</div>
 			</div>
 		)
 	}
