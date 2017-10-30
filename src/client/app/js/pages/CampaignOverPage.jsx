20161d7bb52972fda2cca20177445a987730bb37
fongelias <fongelias@gmail.com>
2017-10-30 11:08:56 -0400

    Added CampaignOverPage for when the current date is past the campaign end date. Routing is in App.jsx as well as Body.jsx

---
+++ src/client/app/js/pages/CampaignOverPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const CampaignOverPage = () => (
+	<div className="pageBody">
+		<div className="card blue">
+			<h1>The interviews for this season are finished</h1>
+		</div>
+		<div className="card center">
+			<p>
+				If you think this is incorrect, please contact <a href="mail://andrew_chang@symantec.com">andrew_chang@symantec.com</a>
+			</p>
+		</div>
+		<div className="card"></div>
+	</div>
+)
+
+export default CampaignOverPage;
