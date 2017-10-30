9a0b15842a168b981186f0a311f86787e45d24ec
fongelias <fongelias@gmail.com>
2017-10-30 10:39:19 -0400

    Added endDate constant and string to exposed module

--- src/client/app/js/constants.js
+++ src/client/app/js/constants.js
@@ -1,9 +1,25 @@
+//Computation for consts
+//Date
+const endDate = new Date("11/27/2017");
+const endDateString = endDate.toLocaleDateString("en-US", {
+	weekday: "long",
+	day: "numeric",
+	month: "long",
+});
+
+
+//Exposed Module
 const constants = {
 	dashboardPage: 'DASHBOARDPAGE',
 	landingPage: 'LANDINGPAGE',
 	registrationSuccessPage: 'REGISTRATIONSUCCESSPAGE',
+	campaignOverPage: 'CAMPAIGNOVERPAGE',
 	userRequests: 'https://widwml054h.execute-api.us-east-1.amazonaws.com/CandidateRegistrationStage/user',
 	problemRequests: 'https://widwml054h.execute-api.us-east-1.amazonaws.com/CandidateRegistrationStage/problems',
+	campaign: {
+		endDate,
+		endDateString,
+	},
 }
 
 
