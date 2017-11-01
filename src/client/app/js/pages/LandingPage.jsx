c035ae833b26271d745cec0b138fc338280694fe
fongelias <fongelias@gmail.com>
2017-11-01 16:35:29 -0400

    updated text

--- src/client/app/js/pages/LandingPage.jsx
+++ src/client/app/js/pages/LandingPage.jsx
@@ -9,7 +9,8 @@ export default class LandingPage extends Component {
 		return (
 			<div className="pageBody landingPage">
 				<div className="card blue">
-					<h1>Symantec Cambridge, Norton Cloud Services Team</h1>
+					<h1>Symantec Cambridge</h1>
+					<h1>Norton Cloud Services Team</h1>
 					<p>2018 Summer Internship Online Coding Problem</p>
 				</div>
 				<div className="card">
@@ -17,6 +18,7 @@ export default class LandingPage extends Component {
 					<p>The first step in our interview process will be two take-home coding exercises.</p>
 					<p>The second step is a one-hour onsite interview (remote is also possible for students unable to visit our Cambridge office).</p>
 					<p>Please register below to receive your personalized link containing further instructions.</p>
+					<p className="text italic">Eligibility: You must be a full-time student in fall of 2018 who is also graduating by the summer of 2018 in order to participate in our internship</p>
 					<p className="text italic">Deadline: the Online Coding Problem portion of our 2018 Summer Internship search will be closing on {constants.campaign.endDateString}</p>
 					<p>Thanks!</p>
 					<RegistrationForm registerUser={this.props.registerUser}/>
--- src/client/app/js/pages/RegistrationSuccessPage.jsx
+++ src/client/app/js/pages/RegistrationSuccessPage.jsx
@@ -10,11 +10,11 @@ export default class RegistrationSuccessPage extends Component {
 			<div className="pageBody registrationSuccessPage">
 				<div className="card blue">
 					<h1>Symantec Interview Portal</h1>
-					<p>You have successfully registered for an account.</p>
+					<p>Thank you!</p>
 				</div>
 				<div className="card center">
 					<p>Please check your email, <span className="text bold">{this.props.user.email}</span> for an invitation with a link to your personal page for next steps.</p>
-					<p>If you do not see our email in your inbox, please check your span folder. Thanks!</p>
+					<p>If you do not see our email in your inbox, please check your spam folder. Thanks!</p>
 				</div>
 			</div>
 		);
