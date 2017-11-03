8366db1c86761addb45a95fbc7cf9d0c2df26072
fongelias <fongelias@gmail.com>
2017-11-03 15:11:27 -0400

    Added visibility API constants and constant determination logic

--- src/client/app/js/constants.js
+++ src/client/app/js/constants.js
@@ -8,6 +8,23 @@ const endDateString = endDate.toLocaleDateString("en-US", {
 });
 
 
+//Visibility API
+// Set the name of the hidden property and the change event for visibility
+let hidden = "";
+let visibilityChange = ""; 
+
+if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support 
+  hidden = "hidden";
+  visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+  hidden = "msHidden";
+  visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+  hidden = "webkitHidden";
+  visibilityChange = "webkitvisibilitychange";
+}
+
+
 //Exposed Module
 const constants = {
 	dashboardPage: 'DASHBOARDPAGE',
@@ -19,6 +36,10 @@ const constants = {
 	campaign: {
 		endDate,
 		endDateString,
+	},
+	visibilityAPI: {
+		hidden,
+		visibilityChange,
 	},
 }
 
